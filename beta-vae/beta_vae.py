596920814dadc9d10c55f902ce41119deda38b77
yixin <yixin.su11@gmail.com>
2018-10-17 12:12:57 +1100

    viz_step50

--- beta-vae/beta_vae.py
+++ beta-vae/beta_vae.py
@@ -67,7 +67,7 @@ flags.DEFINE_string(
     default=os.path.join(os.getenv("TEST_TMPDIR", "/tmp"), "beta-vae/"),
     help="Directory to put the model's fit.")
 flags.DEFINE_integer(
-    "viz_steps", default=5, help="Frequency at which to save visualizations.")
+    "viz_steps", default=50, help="Frequency at which to save visualizations.")
 flags.DEFINE_bool(
     "fake_data",
     default=False,
